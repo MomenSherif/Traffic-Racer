f9a2be206c2733a416e51a3a8579b0dc68c9fcbc
MomenSherif <momensherif.2019@gmail.com>
2020-01-09 16:50:54 +0200

    removed duplicated js code in main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ $(document).on('keydown',function(e)
         {
             $(".players-sec").css('display','block');
         }
-        
+
     }
 });
 $('#btn-start').on('click',function()
@@ -58,14 +58,14 @@ $('#btn-start').on('click',function()
     }
     if(count === currentPlayer)
     {
-        $(".players-sec").css('display','none');   
+        $(".players-sec").css('display','none');
         $("#raceSec").css('display','block');
         let distance = 11;
-        let x = setInterval(function() 
+        let x = setInterval(function()
        {
          distance -= 1;
          $('#count').text(distance);
-         if (distance < 0) 
+         if (distance < 0)
             {
             $('#count').text('');
             audio.play();
@@ -90,110 +90,7 @@ $('#btn-start').on('click',function()
             $('#car-1').css('background-color',players[0].favColor);
             $('#plData-1').find('p').text(players[0].Playername);
             $("#container").css('left',40+"%");
-        }       
+        }
 
     }
-});
-
-/////////////////////////////////////////////////////////////
-const data = {
-    carSpeed: 5,
-  }
-  
-  const Car = function (selector) {
-    this.el = $(selector);
-  
-    this.ableToMove = {
-      left: true,
-      right: true,
-      up: true,
-      down: true
-    }
-  }
-  audio.addEventListener('ended',function()
-  {
-  Car.prototype.moveLeft = function () {
-    this.el.css('left', `-=${data.carSpeed}`);
-    this.ableToMove.left = requestAnimationFrame(this.moveLeft.bind(this));
-  }
-  
-  Car.prototype.stopLeft = function () {
-    cancelAnimationFrame(this.ableToMove.left);
-    this.ableToMove.left = true;
-  }
-  
-  Car.prototype.moveRight = function () {
-    this.el.css('left', `+=${data.carSpeed}`);
-    this.ableToMove.right = requestAnimationFrame(this.moveRight.bind(this));
-  }
-  
-  Car.prototype.stopRight = function () {
-    cancelAnimationFrame(this.ableToMove.right);
-    this.ableToMove.right = true;
-  }
-  
-  Car.prototype.moveUp = function () {
-    this.el.css('top', `-=${data.carSpeed}`);
-    this.ableToMove.up = requestAnimationFrame(this.moveUp.bind(this));
-  }
-  
-  Car.prototype.stopUp = function () {
-    cancelAnimationFrame(this.ableToMove.up);
-    this.ableToMove.up = true;
-  }
-  
-  Car.prototype.moveDown = function () {
-    this.el.css('top', `+=${data.carSpeed}`);
-    this.ableToMove.down = requestAnimationFrame(this.moveDown.bind(this));
-  }
-  
-  Car.prototype.stopDown = function () {
-    cancelAnimationFrame(this.ableToMove.down);
-    this.ableToMove.down = true;
-  }
-  
-  
-  const car = new Car('#car-1');
-  const car2 = new Car('#car-2');
-  
-  $(document).on('keydown', function (e) {
-    if (e.keyCode === 37 && car.ableToMove.left === true) {
-      car.moveLeft();
-    } else if (e.keyCode === 39 && car.ableToMove.right === true) {
-      car.moveRight();
-    } else if (e.keyCode === 38 && car.ableToMove.up === true) {
-      car.moveUp();
-    } else if (e.keyCode === 40 && car.ableToMove.down === true) {
-      car.moveDown();
-    } else if (e.keyCode === 65 && car2.ableToMove.left === true) {
-      car2.moveLeft();
-    } else if (e.keyCode === 68 && car2.ableToMove.right === true) {
-      car2.moveRight();
-    } else if (e.keyCode === 87 && car2.ableToMove.up === true) {
-      car2.moveUp();
-    } else if (e.keyCode === 83 && car2.ableToMove.down === true) {
-      car2.moveDown();
-    }
-  });
-  
-  $(document).on('keyup', function (e) {
-    if (e.keyCode === 37) {
-      car.stopLeft();
-    } else if (e.keyCode === 39) {
-      car.stopRight();
-    } else if (e.keyCode === 38) {
-      car.stopUp();
-    } else if (e.keyCode === 40) {
-      car.stopDown();
-    } else if (e.keyCode === 65) {
-      car2.stopLeft();
-    } else if (e.keyCode === 68) {
-      car2.stopRight();
-    } else if (e.keyCode === 87) {
-      car2.stopUp();
-    } else if (e.keyCode === 83) {
-      car2.stopDown();
-    }
-  
-  });
 });
